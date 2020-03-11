7493ca5cfa29b2e743c0e686c7281a3bd21ee34e
mihael <mihael@avisec.ch>
2020-03-11 21:30:27 +0100

    dodan gps

--- app/src/main/java/com/example/myapplication/AboutActivity.java
+++ app/src/main/java/com/example/myapplication/AboutActivity.java
@@ -1,18 +1,61 @@
 package com.example.myapplication;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 public class AboutActivity extends AppCompatActivity {
+
+    private FusedLocationProviderClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        requestPermission();
+        client = LocationServices.getFusedLocationProviderClient(this);
+
+        Button button = findViewById(R.id.getLocation);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (ActivityCompat.checkSelfPermission(AboutActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+
+                client.getLastLocation().addOnSuccessListener(AboutActivity.this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if (location != null) {
+                            TextView textView = findViewById(R.id.location);
+                            double lat = location.getLatitude();
+                            double lon = location.getLongitude();
+                            double alt = location.getAltitude();
+                            long stmp = location.getTime();
+                            java.util.Date time = new java.util.Date(stmp);
+                            textView.setText(lat + "\n" + lon + "\n" + alt+ "\n" + time);
+                        }
+                    }
+                });
+            }
+        });
 
         ImageView imageView6 = findViewById(R.id.imageView6);
         imageView6.setOnClickListener(new View.OnClickListener() {
@@ -23,6 +66,10 @@ public class AboutActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{ACCESS_FINE_LOCATION}, 1);
     }
 
 }
