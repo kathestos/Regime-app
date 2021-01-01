79c93ea7b31ae6f33d866884df4029ef0717dc96
kathestos <mihael.fucek@gmail.com>
2021-01-01 15:27:29 +0100

    dodana 3 pickera

--- app/src/main/java/com/example/myapplication/TodoActivity.java
+++ app/src/main/java/com/example/myapplication/TodoActivity.java
@@ -1,9 +1,12 @@
 package com.example.myapplication;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.NumberPicker;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +14,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.util.Objects;
 
 public class TodoActivity extends AppCompatActivity {
+
+    SharedPreferences sp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +38,67 @@ public class TodoActivity extends AppCompatActivity {
             String itemValue = (String) listView.getItemAtPosition(position);
             txt.setText(itemValue);
         });
+
+        sp = getSharedPreferences("MyUserPrefs", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+
+        NumberPicker orahPick = findViewById(R.id.pickerOrah);
+        TextView orahText = findViewById(R.id.textOrah);
+        String[] orasi = getResources().getStringArray(R.array.nuts);
+        orahPick.setMinValue(0);
+        orahPick.setMaxValue(orasi.length - 1);
+        orahPick.setDisplayedValues(orasi);
+
+        orahPick.setOnValueChangedListener((numberPicker, oldValue, newValue) -> {
+            int foodNumber = orahPick.getValue();
+            orahText.setText(orasi[foodNumber]);
+            editor.putInt("orah", foodNumber);
+            editor.apply();
+        });
+
+        NumberPicker sjemenkePick = findViewById(R.id.pickerSjemenke);
+        TextView sjemenkeText = findViewById(R.id.textSjemenke);
+        String[] sjemenke = getResources().getStringArray(R.array.seeds);
+        sjemenkePick.setMinValue(0);
+        sjemenkePick.setMaxValue(sjemenke.length - 1);
+        sjemenkePick.setDisplayedValues(sjemenke);
+
+        sjemenkePick.setOnValueChangedListener((numberPicker, oldValue, newValue) -> {
+            int foodNumber = sjemenkePick.getValue();
+            sjemenkeText.setText(sjemenke[foodNumber]);
+            editor.putInt("sjemenke", foodNumber);
+            editor.apply();
+        });
+
+        NumberPicker suhoPick = findViewById(R.id.pickerSuho);
+        TextView suhoText = findViewById(R.id.textSuho);
+        String[] suho = getResources().getStringArray(R.array.dried);
+        suhoPick.setMinValue(0);
+        suhoPick.setMaxValue(suho.length - 1);
+        suhoPick.setDisplayedValues(suho);
+
+        suhoPick.setOnValueChangedListener((numberPicker, oldValue, newValue) -> {
+            int foodNumber = suhoPick.getValue();
+            suhoText.setText(suho[foodNumber]);
+            editor.putInt("suho", foodNumber);
+            editor.apply();
+        });
+
+        SharedPreferences sp2 = getApplicationContext().getSharedPreferences("MyUserPrefs", Context.MODE_PRIVATE);
+
+        int orahNum = sp2.getInt("orah", 0);
+        String savedOrah = orasi[orahNum];
+        orahText.setText(savedOrah);
+        orahPick.setValue(orahNum);
+
+        int sjemenkeNum = sp2.getInt("sjemenke", 0);
+        String savedSjemenke = sjemenke[sjemenkeNum];
+        sjemenkeText.setText(savedSjemenke);
+        sjemenkePick.setValue(sjemenkeNum);
+
+        int suhoNum = sp2.getInt("suho", 0);
+        String savedSuho = suho[suhoNum];
+        suhoText.setText(savedSuho);
+        suhoPick.setValue(suhoNum);
     }
 }
